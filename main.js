93acdeb67185c3a7a7aa23c8443f3b6b72f84913
Amanda Heideman <heideman.amanda@gmail.com>
2021-03-07 00:51:36 +0100

    add filter function to recipes

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
+filter.onchange = function () {
+  let recipes = document.getElementsByClassName("recipe");
+  let selectedFilter = document.getElementById("filter").value;
 
-/* 
-let content = document.getElementById("recipes");
-
-let div = document.createElement("div");
-
-
-content.appendChild(div); */
-
-
+  for (i = 0; i < recipes.length; i++) {
+    allergy = recipes[i].getElementsByTagName("span");
+    txtValue = allergy[0].innerHTML;
+    let txtValue2;
+    if (allergy[1]) {
+      txtValue2 = allergy[1].innerHTML;
+    } else {
+      txtValue2 = allergy[0].innerHTML;
+    }
+    if (selectedFilter === "ShowAll") {
+      recipes[i].style.display = "";
+    } else if (txtValue.indexOf(selectedFilter) > -1) {
+      recipes[i].style.display = "";
+    } else if (txtValue2.indexOf(selectedFilter) > -1) {
+      recipes[i].style.display = "";
+    } else {
+      recipes[i].style.display = "none";
+    }
+  }
+};
